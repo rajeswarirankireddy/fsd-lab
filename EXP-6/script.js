1aa33af3f116c57814bbbfff7342284d4c73510f
rajeswarirankireddy <151986881+rajeswarirankireddy@users.noreply.github.com>
2025-09-24 12:33:32 +0530

    Add files via upload

---
+++ EXP-6/script.js
@@ -0,0 +1,17 @@
+
+swathi srikakulapu
+12:20 PM (0 minutes ago)
+to me
+
+function externalGreeting() {
+    alert("Hello from the external JavaScript file!");
+}
+
+function addNumbers() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let sum = Number(num1) + Number(num2);
+
+    alert("Sum (after type conversion) = " + sum);
+}
